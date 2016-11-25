a0867ad04d402e24e2eec884f7356c9bfdadf91b
Jean-Francois Dockes <jf@dockes.org>
2016-11-25 10:46:46 +0100

    comments

--- libupnpp/upnpplib.hxx
+++ libupnpp/upnpplib.hxx
@@ -25,7 +25,12 @@
 
 namespace UPnPP {
 
-/** Our link to libupnp. Initialize and keep the handle around */
+/** Our link to libupnp. Initialize and keep the handle around.
+ * 
+ * Not all applications will need to use this. The initialization call will be
+ * performed internally as needed, you only need to call it if you need to set 
+ * specific parameters.
+ */
 class LibUPnP {
 public:
     ~LibUPnP();
@@ -34,6 +39,12 @@ public:
      *
      * This initializes libupnp, possibly setting an address and port, possibly
      * registering a client if serveronly is false.
+     *
+     * Note: libupnp uses a single v4 adress interface (+a v6 one if
+     * enabled). This can be specified in the initialization call,
+     * else it will pick the first non-loopback one. There is no way
+     * to have a single instance of the library listen on multiple
+     * interfaces.
      *
      * @param serveronly no client init
      * @param hwaddr returns the hardware address for the specified network
@@ -57,16 +68,33 @@ public:
     /** Returns something like "libupnpp 0.14.0 libupnp x.y.z" */
     static std::string versionString();
 
+    /** libupnp (pupnp) logging: this is distinct from libupnpp logging */
+    /** libupnp log levels */
     enum LogLevel {LogLevelNone, LogLevelError, LogLevelDebug};
-    /** Set libupnp log file name and activate logging.
-     * This does nothing if libupnp was built with logging disabled.
+    /** Set libupnp log file name and activate/deactivate logging.
      *
-     * @param fn file name to use. Use empty string to turn logging off
+     * This does nothing if libupnp was built with logging
+     * disabled. It may be useful for debugging in some case. You
+     * should not use the same file as the one used for libupnpp
+     * logging.
+     *
+     * @param fn file name to use. Use empty string to turn logging off.
      */
     bool setLogFileName(const std::string& fn, LogLevel level = LogLevelError);
+    /** Set libupnp log level. 
+     *
+     * @return true if libupnp logging is enabled, else false.
+     */
     bool setLogLevel(LogLevel level);
 
-    /** Set max library buffer size for reading content from servers. */
+    /** Set max library buffer size for reading content from servers. 
+     * 
+     * Just calls libupnp UpnpSetMaxContentLength(). This defines the maximum
+     * amount of data that Soap calls will accept from the remote. The
+     * libupnp default is very low (16KB), but libupnpp sets the value
+     * to 2MB during initialization. You can reset the value to your
+     * own choice by using this method.
+     */
     void setMaxContentLength(int bytes);
 
     /** Check state after initialization */
@@ -90,12 +118,12 @@ public:
      * general.
      */
 
-    /** Specify function to be called on given UPnP event. This will happen
-     * in the libupnp thread context.
+    /** Private: specify function to be called on given UPnP
+     *  event. This will happen in the libupnp thread context.
      */
     void registerHandler(Upnp_EventType et, Upnp_FunPtr handler, void *cookie);
 
-    /** Translate libupnp event type as string */
+    /** Private: translate libupnp event type to string */
     static std::string evTypeAsString(Upnp_EventType);
 
     int setupWebServer(const std::string& description, UpnpDevice_Handle *dvh);
