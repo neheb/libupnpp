cde74e29a8531f3974139a7fee68ab0b9dbdc61d
Jean-Francois Dockes <jf@dockes.org>
2018-09-15 09:32:11 +0200

    Bumped package version to 0.17 and .so to 8

--- libupnpp/upnpplib.hxx
+++ libupnpp/upnpplib.hxx
@@ -25,7 +25,7 @@
 
 /** Version components. */
 #define LIBUPNPP_VERSION_MAJOR 0
-#define LIBUPNPP_VERSION_MINOR 16
+#define LIBUPNPP_VERSION_MINOR 17
 #define LIBUPNPP_VERSION_REVISION 0
 /// Got this from Xapian...
 #define LIBUPNPP_AT_LEAST(A,B,C)                                        \
