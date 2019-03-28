cdca67131e4b82673aae4ab2ef1fc92a0e19466f
Jean-Francois Dockes <jf@dockes.org>
2019-03-28 09:35:33 +0100

    bumped version to 0.17.1

--- windows/config_windows.h
+++ windows/config_windows.h
@@ -69,7 +69,7 @@
 #define PACKAGE_NAME "libupnpp"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libupnpp 0.17.0"
+#define PACKAGE_STRING "libupnpp 0.17.1"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "libupnpp"
@@ -78,7 +78,7 @@
 #define PACKAGE_URL "http://www.lesbonscomptes.com/upmpdcli"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.17.0"
+#define PACKAGE_VERSION "0.17.1"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
