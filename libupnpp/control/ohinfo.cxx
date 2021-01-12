6be70792b0df06f9c6ca7654f12a80e16afa5a4c
Jean-Francois Dockes <jf@dockes.org>
2021-01-12 10:03:18 +0100

    Control: implement the missing actions in the OHInfo service

--- libupnpp/control/ohinfo.cxx
+++ libupnpp/control/ohinfo.cxx
@@ -92,7 +92,7 @@ int OHInfo::metatext(UPnPDirObject *dirent)
     }
     string didl;
     if (!data.get("Value", &didl)) {
-        LOGERR("OHInfo::Read: missing Value in response" << endl);
+        LOGERR("OHInfo::metatext: missing Value in response" << endl);
         return UPNP_E_BAD_RESPONSE;
     }
     if (didl.empty()) {
@@ -100,6 +100,121 @@ int OHInfo::metatext(UPnPDirObject *dirent)
         return UPNP_E_SUCCESS;
     }
     return OHRadio::decodeMetadata("OHInfo::metatext", didl, dirent);
+}
+
+int OHInfo::track(std::string *uri, UPnPDirObject *dirent)
+{
+    SoapOutgoing args(getServiceType(), "Counters");
+    SoapIncoming data;
+    int ret = runAction(args, data);
+    if (ret != UPNP_E_SUCCESS) {
+        LOGERR("OHInfo::counters: runAction failed\n");
+        return ret;
+    }
+    if (uri) {
+        if (!data.get("Uri", uri)) {
+            LOGERR("OHInfo::track: missing Uri in response" << endl);
+            return UPNP_E_BAD_RESPONSE;
+        }
+    }
+    if (dirent) {
+        string didl;
+        if (!data.get("Metadata", &didl)) {
+            LOGERR("OHInfo::track: missing Metadata in response" << endl);
+            return UPNP_E_BAD_RESPONSE;
+        }
+        return OHRadio::decodeMetadata("OHInfo::metatext", didl, dirent);
+    }
+    return UPNP_E_SUCCESS;
+}
+
+int OHInfo::counters(int *trackcount, int *detailscount, int *metatextcount)
+{
+    SoapOutgoing args(getServiceType(), "Counters");
+    SoapIncoming data;
+    int ret = runAction(args, data);
+    if (ret != UPNP_E_SUCCESS) {
+        LOGERR("OHInfo::counters: runAction failed\n");
+        return ret;
+    }
+    if (trackcount) {
+        const char *nm = "TrackCount";
+        if (!data.get(nm, trackcount)) {
+            LOGERR("OHInfo::counters: missing " << nm << " in response" << endl);
+            return UPNP_E_BAD_RESPONSE;
+        }
+    }
+    if (detailscount) {
+        const char *nm = "DetailsCount";
+        if (!data.get(nm, detailscount)) {
+            LOGERR("OHInfo::counters: missing " << nm << " in response" << endl);
+            return UPNP_E_BAD_RESPONSE;
+        }
+    }
+    if (metatextcount) {
+        const char *nm = "MetatextCount";
+        if (!data.get(nm, metatextcount)) {
+            LOGERR("OHInfo::counters: missing " << nm << " in response" << endl);
+            return UPNP_E_BAD_RESPONSE;
+        }
+    }
+    return UPNP_E_SUCCESS;
+}
+
+
+int OHInfo::details(int *duration, int *bitrate, int *bitdepth, int *samplerate, bool *lossless,
+                    std::string *codecname)
+{
+    SoapOutgoing args(getServiceType(), "Details");
+    SoapIncoming data;
+    int ret = runAction(args, data);
+    if (ret != UPNP_E_SUCCESS) {
+        LOGERR("OHInfo::details: runAction failed\n");
+        return ret;
+    }
+    if (duration) {
+        const char *nm = "Duration";
+        if (!data.get(nm, duration)) {
+            LOGERR("OHInfo::counters: missing " << nm << " in response" << endl);
+            return UPNP_E_BAD_RESPONSE;
+        }
+    }
+    if (bitrate) {
+        const char *nm = "BitRate";
+        if (!data.get(nm, bitrate)) {
+            LOGERR("OHInfo::counters: missing " << nm << " in response" << endl);
+            return UPNP_E_BAD_RESPONSE;
+        }
+    }
+    if (bitdepth) {
+        const char *nm = "BitDepth";
+        if (!data.get(nm, bitdepth)) {
+            LOGERR("OHInfo::counters: missing " << nm << " in response" << endl);
+            return UPNP_E_BAD_RESPONSE;
+        }
+    }
+    if (samplerate) {
+        const char *nm = "SampleRate";
+        if (!data.get(nm, samplerate)) {
+            LOGERR("OHInfo::counters: missing " << nm << " in response" << endl);
+            return UPNP_E_BAD_RESPONSE;
+        }
+    }
+    if (lossless) {
+        const char *nm = "Lossless";
+        if (!data.get(nm, lossless)) {
+            LOGERR("OHInfo::counters: missing " << nm << " in response" << endl);
+            return UPNP_E_BAD_RESPONSE;
+        }
+    }
+    if (codecname) {
+        const char *nm = "Codecname";
+        if (!data.get(nm, codecname)) {
+            LOGERR("OHInfo::counters: missing " << nm << " in response" << endl);
+            return UPNP_E_BAD_RESPONSE;
+        }
+    }
+    return UPNP_E_SUCCESS;
 }
 
 } // End namespace UPnPClient
